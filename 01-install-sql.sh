8863ebdf9176c22dd60fb828ecc97ac3d8000f8c
BharaniProjects <bharani471@gmail.com>
2025-11-26 11:51:10 +0530

    created install sql script

---
+++ 01-install-sql.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+R=\n[31m
+G=\n[32m
+Y=\n[33m
+N=\n[0n
+ID=$(id -u)
+if [ $ID -ne 0 ]
+then
+  echo -e "ERROR :$R Please run with root user.$N"
+  exit 1
+else
+    echo "$G You are root user.$N"
+fi
+dnf install mysql
+if [ $? -ne 0 ]
+then
+    echo "$R Installing mysql failed.$N"
+    exit 1
+else
+    echo "$G Installing mysql is success.$N"
