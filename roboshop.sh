41991561863be10d848711be5611c7682cfdfebd
BharaniProjects <bharani471@gmail.com>
2025-11-29 15:00:28 +0530

    created new script

---
+++ roboshop.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+AMI_ID=ami-0b4f379183e5706b9
+SG_ID=sg-0566f9bf056303113
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+
+for i in ${INSTANCES[@]}
+do
+    if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
+    then
+        INSTANCE_TYPE ="t3.small"
+    else
+        INSTANCE_TYPE="t2.micro"
+    fi
+    aws ec2 run-instances --image-id $AMI_ID --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID
+done
+ 
